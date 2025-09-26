ab7f722fb5346791276ef310455ddb97bf4b3b3f
RISHI <RISHI@RISHIKESAVAN-H>
2025-09-26 20:30:17 +0530

    this is the sample java project

---
+++ TNSIF_JAVA_RISHI/src/com/tnsif/basics/AdditionScanner.java
@@ -0,0 +1,17 @@
+package com.tnsif.basics;
+import java.util.*;
+
+public class AdditionScanner {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the num:");
+		// TODO Auto-generated method stub
+		 int a=s.nextInt();
+		   int b=s.nextInt();
+			   int c=a+b;
+			System.out.println("addition of a="+a+" and b="+b+" is "+c);
+
+	}
+
+}
---
+++ TNSIF_JAVA_RISHI/src/com/tnsif/entityclass/CustomerDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.entityclass;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Customer c1=new Customer();
+       c1.setId(101);
+       c1.setCname("rishi");
+       c1.setCity("vpm");
+    System.out.println(c1);
+       Customer c2=new Customer();
+       c2.setId(102);
+       c2.setCname("kesavan");
+       c2.setCity("vnr");
+    System.out.println(c2);
+	}
+
+}
