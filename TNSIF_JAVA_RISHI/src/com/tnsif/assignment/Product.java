ba41ba7ddef6ab6ef8e9c7470646e627f35479fa
Rishikesavan-124 <rishihariambika@gmail.com>
2025-10-03 20:29:01 +0530

    non access modifiers

---
+++ TNSIF_JAVA_RISHI/src/com/tnsif/assignment/Product.java
@@ -0,0 +1,26 @@
+package com.tnsif.assignment;
+
+public class Product {
+	private int id;
+	private String cname;
+	private int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCprice() {
+		return price;
+	}
+	public void setCprice(String price) {
+		this.price = price;
+	}
+	
+}
---
+++ TNSIF_JAVA_RISHI/src/com/tnsif/assignment/ProductDemo.java
@@ -0,0 +1,28 @@
+package com.tnsif.assignment;
+
+public class ProductDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Product  c1=new Product();
+				c1.getId(101);
+				c1.getCname("Laptop");
+				c1.getCprice("50000")
+			System.out.println(c1);
+				Product c2=new Product();
+				c2.getId(102);
+				c2.getCname("mobile");
+				c2.getCprice("15000");
+			System.out.println(c2);
+				Product c3=new Product();
+				c3.id = 103;
+				c3.cname = "Headphone";
+				c3.price = "2500";
+			System.out.println(c3);
+				
+				// TODO Auto-generated method stub
+
+			}
+
+		}
+
